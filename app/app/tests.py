376b7df88e7035019837a08ac8975b3dd6230c26
WilfreNjeunwi <daytabrown@gmail.com>
2024-03-05 21:23:53 +0100

    missing whitespace

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,13 +10,14 @@ class CalcTests(SimpleTestCase):
 
     def test_add_nmbers(self):
         """Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
+
 
